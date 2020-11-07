6e14d63443616e06d4c97f4f549b991c6809ba30
Esteban Juarez <58830440+ElStevie@users.noreply.github.com>
2020-11-06 20:58:45 -0600

    Users Controller created

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    private $validations;
+
+    function __construct() {
+        $this->validations = [
+            'name' => "required",
+            "email" => "email:rfc",
+            "password" => "required|string"
+        ];
+    }
+
+    public function index()
+    {
+        $users = User::get();
+        return view("users/index", compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("/users/createForm");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate($this->validations);
+
+        User::create($request->all());
+
+        return redirect("/users");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return view("/users/show", compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        return view("users.editForm", compact("user"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate($this->validations);
+        User::where("id", $user->id)->update($request->except("_token", "_method"));
+        return redirect()->route("users.show", [$user->id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route("users.index");
+    }
+}
