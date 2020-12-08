5e8e6ffc1c111cf0c7e004bd39ed81bb7be0ac09
Esteban Juarez <58830440+ElStevie@users.noreply.github.com>
2020-12-08 06:15:11 -0600

    Removed nav bar, added breadcrumb in 'app' layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,12 +20,12 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-dropdown')
+
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+                    <a class="text-blue-600 hover:text-blue-400" href="/">Home </a>/ {{ $header }}
                 </div>
             </header>
 
